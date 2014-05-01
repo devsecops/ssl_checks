15e4132ce598d285b147b7e125de065ec24f51c0
devsecops <shannon@trynyty.com>
2014-04-30 21:45:41 -0700

    Update date_check.sh

--- date_check/date_check.sh
+++ date_check/date_check.sh
@@ -1,1 +1,15 @@
-for i in `cat EXTERNAL`; do echo -e "\n----START "$i"----" ; echo | openssl s_client -connect "$i":443 2>/dev/null | openssl x509 -noout -dates; echo -e "----END "$i"----"; done > ssl-Dates-Output.txt
+#!/bin/sh
+
+for i in `cat EXTERNAL`
+do
+   echo "\n----START "$i"----"
+   ping -q -c4 "$i" > /dev/null
+
+   if [ $? -eq 0 ]
+      then
+         echo | openssl s_client -connect "$i":443 2>/dev/null | openssl x509 -noout -dates
+      else
+         echo "     "$i" is not available"
+   fi
+   echo "----END "$i"----"
+done > ssl_date_check.txt
